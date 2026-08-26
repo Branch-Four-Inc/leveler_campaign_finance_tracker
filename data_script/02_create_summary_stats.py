716376536e816224f469977eb25fbc43f560997a
Stella Mach <smach@branchfour.org>
2026-08-26 11:15:41 -0400

    update code to include money by share; create share data files

--- data_script/02_create_summary_stats.py
+++ data_script/02_create_summary_stats.py
@@ -8,42 +8,16 @@ Created on Wed Aug 19 21:08:52 2026
 import pandas as pd 
 import numpy as np 
 
+
 input_dir = 'C:\\Users\\stm4z\\OneDrive - branchfour.org\\Local Data Lab\\Repositories\\leveler_campaign_finance_tracker\\data_output\\'
 
 
-total_contributions = pd.read_csv(input_dir + 'total_contributions.csv')
 
-# Total contribution by election
-total_contributions_by_election = (total_contributions.groupby(['Location', 'Office'])['Total Contributions']
-                                   .sum()
-                                   .reset_index(drop=False)
-                                   .sort_values(['Location', 'Office']))
-
-# most pac money 
-contributor_type = pd.read_csv(input_dir + 'contributor_types.csv')
-
-most_pac_money = (contributor_type[contributor_type['Contributor Type'].str.lower().str.contains('pac|political action committee')]
-                  .sort_values('Total Contributions', ascending = False)
-                  .head(1) )
-
-# most corporate money
-#contributor_type = pd.read_csv(input_dir + 'contributor_types.csv')
-
-most_corporate_money = (contributor_type[contributor_type['Contributor Type'].str.lower().str.contains('partnership|professional|limited liability company')]
-                  .sort_values('Total Contributions', ascending = False)
-                  .head(1) )
-
-# most out of state 
-in_state = pd.read_csv(input_dir + 'instate_perc.csv')
-
-most_out_state_money = (in_state[in_state['Contributor Location'].str.lower() =='in-state']
-                  .sort_values('Amount', ascending = False)
-                  .head(1) )
-
-
-# widest fundraising gap
-
-def top_two_spread(group):
+def top_two_spread(group: pd.DataFrame): 
+    
+    ''' 
+    Calculate election with widest fundraising gap
+    '''
     sorted_group = group.sort_values('Total Contributions', ascending=False)
     top_row = sorted_group.iloc[0]
     
@@ -66,17 +40,108 @@ def top_two_spread(group):
         'Difference': difference
     })
 
-fundraising_gap =  total_contributions.groupby(['Location', 'Office']).apply(top_two_spread).reset_index().sort_values('Difference', ascending = False)
+
+def top_candidate(df: pd.DataFrame, 
+                  variable: str, 
+                  contains_string: str, 
+                  sorting_variable: str):
+    
+    ''' 
+    Identify top candidate for different money categories 
+    '''
+    
+    return (df[df[variable].str.lower().str.contains(contains_string)]
+                      .sort_values(sorting_variable, ascending = False)
+                      .head(1) )
+    
+
+
+pac_string = 'pac|political action committee'
+corporate_string = 'partnership|professional|limited liability company'
+outstate_string = 'out-of-state'
+union_string = 'union'
+
+
+
+total_contributions = pd.read_csv(input_dir + 'total_contributions.csv')
+
+# Total contribution by election
+total_contributions_by_election = (total_contributions.groupby(['Location', 'Office'])['Total Contributions']
+                                   .sum()
+                                   .reset_index(drop=False)
+                                   .sort_values(['Location', 'Office']))
+
+
+# ------------------------------------------------------------------------------------------
+# MOST BY MONEY 
+# ------------------------------------------------------------------------------------------
+
+# most pac money 
+contributor_type = pd.read_csv(input_dir + 'contributor_types.csv')
+
+most_pac_money = top_candidate(contributor_type, 'Contributor Type', pac_string, 'Total Contributions')
+
+# most corporate 
+most_corporate_money = top_candidate(contributor_type, 'Contributor Type', corporate_string, 'Total Contributions')
+
+
+# most out of state 
+in_state = pd.read_csv(input_dir + 'instate_perc.csv')
+
+most_out_state_money = top_candidate(in_state, 'Contributor Location', outstate_string, 'Amount')
+
+
+# widest fundraising gap
+
+fundraising_gap = total_contributions.groupby(['Location', 'Office']).apply(top_two_spread, include_groups=False).reset_index().sort_values('Difference', ascending=False)
+#fundraising_gap =  total_contributions.groupby(['Location', 'Office']).apply(top_two_spread).reset_index().sort_values('Difference', ascending = False)
 widest_gap = fundraising_gap.head(1)
 smallest_gap = fundraising_gap.tail(1)
 
-
+# top fundraiser 
 top_fundraiser = total_contributions.sort_values('Total Contributions', ascending=False).head(1)
 
 
+# ------------------------------------------------------------------------------------------
+# MOST BY SHARE
+# ------------------------------------------------------------------------------------------
+
+# HIGHEST SHARE BY PAC, CORPORATE, UNION 
+
+contribution_share = contributor_type.merge(total_contributions[['Candidate', 'Total Contributions']], how = 'left', on = 'Candidate')
+contribution_share['share'] = contribution_share['Total Contributions_x']/contribution_share['Total Contributions_y']
+
+# pac share 
+pac_share = top_candidate(contribution_share, 'Contributor Type', pac_string, 'share')
+
+# corporate share 
+corporate_share =  top_candidate(contribution_share, 'Contributor Type', corporate_string, 'share')
+
+# union share 
+union_share = top_candidate(contribution_share, 'Contributor Type', union_string, 'share')
+
+# HIGHEST OUT OF STATE SHARE
+
+state_share = in_state.merge(total_contributions[['Candidate', 'Total Contributions']], how = 'left', on = 'Candidate')
+state_share['share'] = state_share['Amount']/state_share['Total Contributions']
+
+outstate_share = top_candidate(state_share, 'Contributor Location', outstate_string, 'share')
+
+
+# ------------------------------------------------------------------------------------------
+# EXPORT OUTPUTS 
+# ------------------------------------------------------------------------------------------
+
+# output totals 
 total_contributions_by_election.to_csv(input_dir + 'summary_page//total_contributions_by_election.csv')
 most_pac_money.to_csv(input_dir + 'summary_page//most_pac_money.csv')
 most_corporate_money.to_csv(input_dir + 'summary_page//most_corporate_money.csv')
 most_out_state_money.to_csv(input_dir + 'summary_page//most_out_state_money.csv')
 widest_gap.to_csv(input_dir + 'summary_page//widest_fundraising_gap.csv')
 smallest_gap.to_csv(input_dir + 'summary_page//smallest_fundraising_gap.csv')
+
+# output shares 
+pac_share.to_csv(input_dir + 'summary_page//most_pac_share.csv')
+corporate_share.to_csv(input_dir + 'summary_page//most_corporate_share.csv')
+union_share.to_csv(input_dir + 'summary_page//most_union_share.csv')
+outstate_share.to_csv(input_dir + 'summary_page//most_out_state_share.csv')
